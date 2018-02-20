a3f025d7efcb11775a7663b195ac557f3e7dc265
Steve Kibe <stevekibe2345@gmail.com>
2018-02-20 12:34:27 +0300

    add the changes made to the text in css and html

--- jss/script.js
+++
@@ -1,9 +0,0 @@
-/* Open the sidenav */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
